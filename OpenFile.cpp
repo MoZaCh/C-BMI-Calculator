f2cafb638f486bc4f1033e1c5d813b14a04db91f
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-03-23 19:41:33 +0000

    Open file function declared

---
+++ OpenFile.cpp
@@ -0,0 +1,9 @@
+#include "OpenFile.h"
+
+void SaveToFile(string name, double bmi) {
+
+}
+
+void readFromFile() {
+
+}
--- OpenFile.h
+++ OpenFile.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+void SaveToFile(string name, double bmi);
+
+void readFromFile();
