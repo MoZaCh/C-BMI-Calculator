4dc9db5cef8682e82cc5f84a5fccccd3d50b2688
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-03-23 19:42:48 +0000

    Open file function defined

--- OpenFile.cpp
+++ OpenFile.cpp
@@ -1,9 +1,30 @@
 #include "OpenFile.h"
 
-void SaveToFile(string name, double bmi) {
+void SaveToFile(string name, double bmi)
+{
+	ofstream file;
+	file.open("BMI_History.txt", std::ios_base::app); //Append to file instead of overwriting
 
+	if (file.is_open()) //Check that the file is open
+	{
+		file << name << " : " << bmi << " : " << __DATE__ << "\n";
+	}
+	file.close(); //Close file
 }
 
-void readFromFile() {
+void readFromFile()
+{
+	cout << "Reading from file..." << endl;
+	fstream file;
+	file.open("BMI_History.txt", ios::in);
 
+	if (file.is_open()) //Check that the file is open
+	{
+		string line;
+		while (getline(file, line))
+		{
+			cout << line << "\n";
+		}
+	}
+	file.close(); //Close file
 }
