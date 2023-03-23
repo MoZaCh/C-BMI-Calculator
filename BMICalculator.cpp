dba99765b04077aee961564b2b2138336d4b8e29
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-03-23 19:32:34 +0000

    BMI Function documentation added

--- BMICalculator.cpp
+++ BMICalculator.cpp
@@ -1,6 +1,12 @@
 #include "BMICalculator.h"
 
-
+/*
+* Body Mass Index (BMI) Calculator
+* 
+* @param height - Takes height as double
+* @param weight - Takes weight as double
+* @return bmi - The given BMI for the values provided
+*/
 double BMICalculator(double height, double weight)
 {
 	double bmi = weight / (pow(height, 2));
