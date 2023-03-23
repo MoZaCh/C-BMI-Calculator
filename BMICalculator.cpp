00e8ac8e3e87ee9c4d2ee3f65af88adf1c4b5a38
Mohammed Zahed Choudhury <mzchoudhury@outlook.com>
2023-03-23 19:29:12 +0000

    BMI Function declared

---
+++ BMICalculator.cpp
@@ -0,0 +1,5 @@
+#include "BMICalculator.h"
+
+double BMICalculator(double height, double weight) {
+	return 0.0;
+}
--- BMICalculator.h
+++ BMICalculator.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+using std::runtime_error;
+using namespace std;
+
+double BMICalculator(double, double );
